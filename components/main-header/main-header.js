cbc4e949d5ecc550fcd51c9aa9e73a930eb9bfad
joydumply <nick.babichev@gmail.com>
2024-05-29 21:02:43 +0300

    29.05.2024 - Added pages templates; Explode Components & Styles modules

---
+++ components/main-header/main-header.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logoImg from '@/assets/logo.png';
+import MainHeaderBackground from '@/components/main-header/main-header-background';
+import classes from './main-header.module.css';
+import NavLink from './nav-link';
+
+export default function MainHeader() {
+	return (
+		<>
+			<MainHeaderBackground />
+			<header className={classes.header}>
+				<Link
+					className={classes.logo}
+					href="/"
+				>
+					<Image
+						src={logoImg}
+						alt="A plate with food on it"
+						priority
+					/>
+					NextLevelFood
+				</Link>
+				<nav className={classes.nav}>
+					<ul>
+						<li>
+							<NavLink href="/meals">Browse Meals</NavLink>
+						</li>
+						<li>
+							<NavLink href="/community">Foodies Community</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</>
+	);
+}
