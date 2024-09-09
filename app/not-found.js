38b9a2c42a64be909b9308778ea8a905155484d7
joydumply <nick.babichev@gmail.com>
2024-09-09 21:46:12 +0300

    preventing cache for dynamic pages

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFoud(){
     return <main className="not-found">
         <h1>Not found</h1>
-        <p>Sorry, we didn't found what you're looking for.</p>
+        <p>Sorry, we did not found what you are looking for.</p>
     </main>
 }
